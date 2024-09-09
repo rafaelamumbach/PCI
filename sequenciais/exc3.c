20b62b858afab9fcad5e2df88885300343586802
Rafaela <seuemail@example.com>
2024-09-09 14:43:29 -0300

    ok

--- sequenciais/exc3.c
+++ sequenciais/exc3.c
@@ -1,6 +1,7 @@
 /*3) Desenvolva um programa que solicite ao usuário digitar dois valores inteiros. Após a digitação, o computador deverá executar a multiplicação destes números e mostrar o resultado na tela do computador.
 
 Exemplo: se o usuário digitar os valores 11 e 13, o programa deve realizar a multiplicação destes números e mostrar na tela o resultado (143).*/
+
 #include <stdio.h>
 
 int main(){
