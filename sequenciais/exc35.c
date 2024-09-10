daeba04203fb6b881595da48aeb7515251842cdc
Rafaela <seuemail@example.com>
2024-09-10 14:51:41 -0300

    ok

---
+++ sequenciais/exc35.c
@@ -0,0 +1,15 @@
+/*35) (S00001400) Desenvolva um programa que solicite ao usuário um horário (hora, minuto, segundo) no formato de 24 horas (faixa de 00:00:00 a 23:59:59) e uma quantidade de segundos transcorridos. O programa deve permitir que o usuário informe o horário através de três informações independentes: hora, minuto e segundo. 
+
+Após a digitação, o computador deverá calcular um segundo horário, que é a soma do primeiro horário e dos segundos transcorridos. O horário calculado deve ser mostrado na tela no formato de 24 horas.
+
+Exemplo: se o usuário digitar como horário inicial os valores 9 (horas), 36 (minutos) e 45 (segundos) e 12.500 segundos transcorridos, o programa deve mostrar na tela 12 horas 46 minutos e 27 segundos. Para efetuar este cálculo, é necessário:
+
+converter a quantidade de horas e minutos do horário informado para segundos:  22*3600 + 15*30 +45 =  33487 segundos
+somar a quantidade de segundos transcorridos:  33.487 segundos +  12.500 segundos = 45.987 segundos
+transformar o horário total em segundos para horas e minutos, utilizando os operadores de divisão que fornecem a parte inteira do quociente e o resto da divisão:*/
+
+#include <stdio.h>
+
+int main() {
+    
+}
