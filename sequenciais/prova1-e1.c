387dd5be5e47c6a7c1bb52ec07e4beef1a8e3987
Rafaela <seuemail@example.com>
2024-10-09 17:10:39 -0300

    início registro p1/PCI

---
+++ sequenciais/prova1-e1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*Um sistema de rastreamento de pacotes utiliza um codigo numerico de 10 digitos, onde os 4o e 5o digitos representam o codigo de localizacao da central de distribuicao. Escreva um programa em linguagem C que leia um numero inteiro de 10 digitos, correspondente ao codigo de rastreamento, e exiba o codigo de localizacao.*/
+
+int main(){
+    int codigo, resto, d4, d5;
+    printf("Informe um nro de 10 digitos: ");
+    scanf("%d", &codigo);
+
+}
