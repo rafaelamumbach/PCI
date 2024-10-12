ffc3033bdc69439d3fdac97297edb215baa5f12c
Rafaela <seuemail@example.com>
2024-10-12 03:59:51 -0300

    segundo exercicio do while (rever) - 12/10

--- repeticao/excw2.c
+++ repeticao/excw2.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
 
-int main(){
-    int codigo=1, votacao;
-    int maiorCodigo, maiorVotacao;
+int main() {
+    int codigo, qtdeVotos;
+    int maiorCodigo = 0, segundoMaiorCodigo = 0;
+    int maiorVotos = 0, segundoMaiorVotos = 0;
 
-    while (codigo != 0){
-        printf("0 p/ parar.");
-        printf("Codigo do candidato: ");
+    do {
+        printf("Digite o codigo do candidato (0 para parar): ");
         scanf("%d", &codigo);
 
-        printf("Qtde votacao: ");
-        scanf("%d", &votacao);
+        if (codigo == 0) {
+            break;
+        }
+
+        printf("Digite a quantidade de votos: ");
+        scanf("%d", &qtdeVotos);
 
 
+        if (qtdeVotos > maiorVotos) {
+            
+            segundoMaiorVotos = maiorVotos;
+            segundoMaiorCodigo = maiorCodigo;
 
+            maiorVotos = qtdeVotos;
+            maiorCodigo = codigo;
+        } 
+        else if (qtdeVotos > segundoMaiorVotos) {
+            segundoMaiorVotos = qtdeVotos;
+            segundoMaiorCodigo = codigo;
+        }
+
+    } while (codigo != 0);
+
+    if (maiorCodigo != 0) {
+        printf("1o Lugar: Candidato %d com %d votos.\n", maiorCodigo, maiorVotos);
+        if (segundoMaiorCodigo != 0) {
+            printf("2o Lugar: Candidato %d com %d votos.\n", segundoMaiorCodigo, segundoMaiorVotos);
+        }
+    } else {
+        printf("Nenhum candidato foi votado.\n");
     }
+
+    return 0;
 }
