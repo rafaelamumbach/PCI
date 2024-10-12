ae8657e8e5f284136cb4e6fb7d3055bd5b1e8666
Rafaela <seuemail@example.com>
2024-10-12 03:42:56 -0300

    primeiro exercicios comandos while

--- repeticao/excw1.c
+++ repeticao/excw1.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
 int main(){
-    
+    int valores;
+    valores = 1;
+    int menor = 100;
+
+    while (valores != 0){
+        printf("Informe um nro (0 p/ parar): ");
+        scanf("%d", &valores);
+
+        if(valores > 0){
+            if(valores < menor){
+                menor = valores;
+            }
+        }
+    }
+
+    printf("Menor nro informado: %d", menor);
 }
---
+++ repeticao/excw2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int codigo=1, votacao;
+    int maiorCodigo, maiorVotacao;
+
+    while (codigo != 0){
+        printf("0 p/ parar.");
+        printf("Codigo do candidato: ");
+        scanf("%d", &codigo);
+
+        printf("Qtde votacao: ");
+        scanf("%d", &votacao);
+
+
+
+    }
+}
