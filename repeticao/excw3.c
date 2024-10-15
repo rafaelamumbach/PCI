afa9f81f596f542567a044deab200ed0379ec1f7
Rafaela <seuemail@example.com>
2024-10-15 11:38:32 -0300

    primeiros exercicios - 15/10 repeticao

---
+++ repeticao/excw3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int numero, i, menor=100, cont=0;
+
+    for (i = 0; i < 20; i++){
+        printf("Informe um nro (%d)/20: ", i+1);
+        scanf("%d", &numero);
+
+        if(numero < menor){
+            menor = numero;
+            cont = 1; //reinicia o contador se encontrou o menor
+
+            if(numero == menor){
+                cont++; //incrementa o contador se o numero atual for igual ao menor 
+            }
+        }
+    }
+
+    printf("O menor nro informado foi %d e apareceu %d vezes.", menor, cont);
+}
