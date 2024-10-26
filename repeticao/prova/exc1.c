89b97142a4d0cde8836f1ca9c2b9868c977078a0
Rafaela <seuemail@example.com>
2024-10-26 11:52:29 -0300

    mudancas exercicio prova

--- repeticao/prova/exc1.c
+++ repeticao/prova/exc1.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
-#include <float.h>  // Para usar a constante FLT_MAX
+#include <float.h>
 
 int main() {
     float diferenca, menor_diferenca = FLT_MAX;
     int posicao, posicao_mais_proxima = 0;
     float diferenca_anterior = 0.0;
 
-    // Leitura da primeira diferença
-    printf("Digite a diferença de tempo (segundos) em relação ao líder (negativo para encerrar): ");
+    
+    printf("Digite a diferenca de tempo (seg) em relacao ao lider (negativo para encerrar): ");
     scanf("%f", &diferenca);
 
-    posicao = 2;  // A primeira posição a ser comparada é a do competidor 2
+    posicao = 2;
 
     while (diferenca >= 0) {
-        if (posicao > 2) {  // Só faz a comparação a partir do competidor 3 em diante
+        if (posicao > 2) {
             float diferenca_atual = diferenca - diferenca_anterior;
             if (diferenca_atual < menor_diferenca) {
                 menor_diferenca = diferenca_atual;
-                posicao_mais_proxima = posicao - 1;  // Guarda a posição anterior, que está mais próxima
+                posicao_mais_proxima = posicao - 1;
             }
         }
 
         diferenca_anterior = diferenca;
-
-        // Leitura da próxima diferença
         printf("Digite a diferença de tempo (segundos) em relação ao líder (negativo para encerrar): ");
         scanf("%f", &diferenca);
 
